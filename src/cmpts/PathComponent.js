bd6fbb99b4a719f3002b5001e9e6d2bdae4b2efe
DESKTOP-ID2KUBF\openworks <jazzyfact95@gmail.com>
2022-03-31 16:51:36 +0900

    backup

--- src/cmpts/PathComponent.js
+++ src/cmpts/PathComponent.js
@@ -1,95 +1,152 @@
-import React, { useEffect } from 'react';
-
-// * 만약 굴곡으로 인하여 정상적인 퍼센테이지 적용이 되지 않을 경우
-//  아래와 같이 조율하여 사용하면 된다.
-// const getDashoffset = (percent) => {
-//   let result = 0;
-//   if (percent <= 10) {
-//     result = 344 - percent * 7.14;
-//   } else if (percent <= 20) {
-//     result = 344 - percent * 5.64;
-//   } else if (percent <= 30) {
-//     result = 344 - percent * 4.7;
-//   } else if (percent <= 40) {
-//     result = 344 - percent * 4.4;
-//   } else if (percent <= 50) {
-//     result = 344 - percent * 4.5;
-//   } else if (percent <= 60) {
-//     result = 344 - percent * 4.4;
-//   } else if (percent <= 70) {
-//     result = 344 - percent * 3.7;
-//   } else if (percent <= 80) {
-//     result = 344 - percent * 3.6;
-//   } else if (percent <= 90) {
-//     result = 344 - percent * 3.44;
-//   } else {
-//     result = 344 - percent * 3.44;
-//   }
-//   return result;
-// };
+import React, { useEffect, useState } from 'react';
 
 const Path = ({ pathOption, percent }) => {
   const {
+    // cover path vaule
     id,
     // id 식별값
-    direction,
-    // 굴진 방향
     path_d_value,
     // path 태그의 속성 d 값
+    direction,
+    // 진행 방향 left || right
     translate_value,
-    // 좌표 값 이동
+    // 좌표 값 이동 (left,top)
     scale_value,
-    // 배율 값
+    // 배율 값 (width, height)
     path_stroke_dasharray_value,
     // 패스의 총 길이
     path_stroke_dashoffset_multiply_value,
     // 패스의 비율 가중치
+
+    // cover path vaule
+    cover_path_fill_value,
+
+    // svg path values
     main_path_stroke_width_value,
     // 메인 패스의 두께
     main_path_stroke_color_value,
     // 메인 패스의 채워진 선의 색상값
     main_path_fill_value,
     // 메인 패스의 채워지지 않은 선의 색상값
+    main_path_opacity,
+
+    // ani path values
+    ani_path_stroke_dasharray_split_value,
+    ani_path_stroke_width_value,
+    ani_path_stroke_color_value,
+    ani_path_fill_value,
+    ani_path_opacity,
+
+    // animation values
+    animation_speed,
+    animation_timing_function,
+    animation_iteration_count,
+
+    // reverse path values
+    reverse_path_stroke_width_value,
+    reverse_path_stroke_color_value,
+    reverse_path_fill_value,
+    reverse_path_opacity,
   } = pathOption;
+
+  const [currentStyle, setCurrentStyle] = useState('');
 
   const calCurrentDashoffset = () => {
     const dasharrayV = path_stroke_dasharray_value;
     const percentV = percent;
     const multiplyV = path_stroke_dashoffset_multiply_value;
 
-    if (direction === 'reverse') {
-      console.log('reverse');
-      console.log(
-        'dashoffset >',
-        percentV * 2 * (dasharrayV / 100 - multiplyV),
+    if (direction === 'right') {
+      // right
+      // right는 전체 dash array 전체길이에 현재 굴진량 만큼의 dash off set 길이를
+      // 더해서 설정한다.
+      // console.log(
+      //   'right, cal current dashoffset >',
+      //   percentV * (dasharrayV / 100) * multiplyV,
+      // );
+      return (
+        path_stroke_dasharray_value + percentV * (dasharrayV / 100) * multiplyV
       );
-      return dasharrayV - percentV * (dasharrayV / 100 - multiplyV);
     }
-    // right
-    console.log(
-      'dashoffset >',
-      dasharrayV - percentV * (dasharrayV / 100 - multiplyV),
-    );
-    return dasharrayV - percentV * (dasharrayV / 100 - multiplyV);
+    // left (default)
+    // left는 전체 dash array 길이에서 현재 굴진량 만큼의 dash off set 길이를
+    // 빼서 설정한다.
+    // console.log(
+    //   'left, cal current dashoffset >',
+    //   dasharrayV - percentV * (dasharrayV / 100) * multiplyV,
+    // );
+    return dasharrayV - percentV * (dasharrayV / 100) * multiplyV;
   };
 
-  const css = `
+  // 현재 굴진거리 이외의 덮어주는 path의 dashoffset값 계산
+  const calReverseDashoffset = () => {
+    if (direction === 'right') {
+      // right
+      return path_stroke_dasharray_value + calCurrentDashoffset();
+    }
+    // left (default)
+    return -(path_stroke_dasharray_value - calCurrentDashoffset());
+  };
+
+  const css_cover_path = `
+  #${id}-cover-path {
+    stroke-dasharray: ${path_stroke_dasharray_value};
+    fill: ${cover_path_fill_value};
+  }`;
+
+  const css_main_path = `
   #${id}-main-path {
     fill: ${main_path_fill_value};
     stroke: ${main_path_stroke_color_value};
     stroke-width: ${main_path_stroke_width_value};
     stroke-dasharray: ${path_stroke_dasharray_value};
     stroke-dashoffset: ${calCurrentDashoffset()};
-  }
-  #${id}-cover-path-line {
-    opacity: 1;
-    stroke: #fff;
-    /* stroke-width: ${path_stroke_dasharray_value}; */
-    stroke-width: 1;
+    opacity: ${main_path_opacity}
+  }`;
+
+  const css_ani_path = `
+  #${id}-ani-path{
+    fill: ${ani_path_fill_value};
+    stroke: ${ani_path_stroke_color_value};
+    stroke-width: ${ani_path_stroke_width_value};
+    stroke-dasharray: ${
+      path_stroke_dasharray_value / ani_path_stroke_dasharray_split_value
+    };
+    stroke-dashoffset: ${calCurrentDashoffset()};
+    stroke: ${ani_path_stroke_color_value};
+    opacity : ${ani_path_opacity};
+    animation-name : ${id}-animation;
+    animation-duration : ${percent / animation_speed}s ;
+    animation-timing-function: ${animation_timing_function};
+    animation-iteration-count : ${animation_iteration_count};
+  }`;
+
+  const css_reverse_path = `
+  #${id}-reverse-path {
+    fill: ${reverse_path_fill_value};
+    stroke: ${reverse_path_stroke_color_value};
+    stroke-width: ${reverse_path_stroke_width_value};
     stroke-dasharray: ${path_stroke_dasharray_value};
-    fill: #fff;
-  }
-`;
+    stroke-dashoffset: ${calReverseDashoffset()};
+    opacity : ${reverse_path_opacity};
+  }`;
+
+  const css_keyframes = `
+  @keyframes ${id}-animation{
+    0%{
+      stroke-dashoffset : ${path_stroke_dasharray_value};
+    }
+    100%{
+      stroke-dashoffset : ${calCurrentDashoffset()};
+    }
+  }`;
+
+  const css =
+    css_cover_path +
+    css_main_path +
+    css_ani_path +
+    css_reverse_path +
+    css_keyframes;
 
   return (
     <>
@@ -98,17 +155,20 @@ const Path = ({ pathOption, percent }) => {
         {/* mask 속성은 url(_) 형식으로 지정할때 id가 아니면 지정이 되지 않는다. */}
         <mask id={`${id}-path-cover-mask`}>
           {/* 내부 percentage 에 따라 변화하는 path를 덮어주는 외부 영역 지정 path */}
-          <path id={`${id}-cover-path-line`} d={path_d_value} />
+          <path id={`${id}-cover-path`} d={path_d_value} />
         </mask>
       </defs>
-      {/* transform 초기 위치 지정, scale 배율지정 */}
       <g
         id={`${id}-main-path-group`}
         transform={`translate(${translate_value}) scale(${scale_value})`}
         mask={`url(#${id}-path-cover-mask)`}
       >
-        {/* 퍼센트에 따라 움직이는 path 노란색 */}
+        {/* 퍼센트에 따라 움직이는 path 입니다. */}
         <path id={`${id}-main-path`} d={path_d_value} />
+        {/* 애니메이션 path 입니다. 전체 path의 총길이를 따라갑니다. */}
+        <path id={`${id}-ani-path`} d={path_d_value} />
+        {/* 현재 퍼센트를 제외하고 잔여 퍼센트 영역을 덮는 reverse path 입니다. */}
+        <path id={`${id}-reverse-path`} d={path_d_value} />
       </g>
     </>
   );
--- src/cmpts/SvgContainer.js
+++ src/cmpts/SvgContainer.js
@@ -2,7 +2,6 @@
 import React, { useCallback, useMemo, useEffect } from 'react';
 import styled, { css } from 'styled-components';
 import PathComponent from './PathComponent';
-import PathAnimation from './PathAnimation';
 
 const SvgArea = styled.div`
   box-sizing: border-box;
@@ -28,6 +27,7 @@ const SvgContainer = ({ percent1, percent2 }) => {
   // 이 총길이는 path_stroke_dasharray_value 의 값으로 대입하시면 됩니다.
   // 만약 document.getElementById(`${id}-main-path`).getTotalLength(), 를 그대로 대입하시게 되면
   // 렌더링 이후에 값을 불러올 수 있으므로 동기를 맞춰주셔야 합니다.
+
   // useEffect(() => {
   // svg path의 총 길이를 알아내기
   // console.log(
@@ -38,6 +38,7 @@ const SvgContainer = ({ percent1, percent2 }) => {
 
   // 색상값 전달할 때 #123444 혹은 red 같은 형식으로 넣어야합니다.
   const pathObjExample = {
+    // [svg common values]
     id: 'loc1-station101',
     // 각 컴포넌트에 svg 요소들을 구분할 수 있는 공통 id 값을 지정합니다.
     path_d_value: `M182.4, ... .4,595.3z`,
@@ -54,21 +55,41 @@ const SvgContainer = ({ percent1, percent2 }) => {
     // svg path의 dashoffset 값은 현재의 퍼센트 만큼 path를 움직이는 효과를 냅니다.
     // 그런데 svg path의 곡률이나, svg에 어떤 예상하지 못한 문제점이 있을때
     // dashoffset 값에 path_stroke_dashoffset_multiply_value 가변적인 값을 곱해 가감/가중 치를 부여해 해결합니다.
+
+    // [cover values]
+    cover_path_fill_value: '#fff',
+    // 하위 path 들의 라인을 지정하는 mask 영역의 path 의 fill 속성입니다.
+    // 전체 path에 대한 투명도를 관장합니다.
+    // 이 속성은 하얀색에 가까워 질 수록 불투명해지고
+    //  검정색에 가까워 질 수록 투명해집니다.
+
+    // [main path values]
     main_path_stroke_width_value: 1,
     // 메인 path의 두께를 지정합니다.
     main_path_stroke_color_value: '#fff',
     // 메인 path의 색상을 지정합니다.
     main_path_fill_value: 'none',
-    // 메인 패스의 채워지지 않은 선의 색상값입니다. "none" 이면 색상을 없앱니다.
+    // 메인 path의 채워지지 않은 선의 색상값입니다. "none" 이면 색상을 없앱니다.
     // transparent 속성은 적용 불가능합니다.
-    // 애니메이션 분리 및 설명 해야함
-    ani_path_stroke_dasharray_value: 1,
-    //
+    main_path_opacity: 1,
+    // 메인 path의 투명도를 지정합니다.
+
+    // [animation values]
+    ani_path_stroke_dasharray_split_value: 100,
+    // 애니메이션 path 의 stroke_dasharray 속성을 나눌 값을 정해줍니다.
+    // 값을 크게 나눌수록 잘게 쪼개집니다.
     ani_path_stroke_width_value: 1,
-    ani_path_stroke_color_value: '#fff',
-    ani_path_fill_value: '#fff',
-    ani_reverse_path_stroke_width_value: 1,
-    ani_reverse_path_stroke_color_value: '#fff',
+    ani_path_stroke_color_value: '#971717',
+    ani_path_fill_value: 'none',
+    ani_path_opacity: 1,
+    ani_path_animation_type: 'linear',
+    ani_path_animation_speed: 1,
+
+    // [reverse path values]
+    reverse_path_stroke_width_value: 1,
+    reverse_path_stroke_color_value: '#fff',
+    reverse_path_fill_value: '#fff',
+    reverse_path_opacity: 1,
   };
   // ======================================
 
@@ -82,29 +103,28 @@ const SvgContainer = ({ percent1, percent2 }) => {
       scale_value: '1, 1',
       path_stroke_dasharray_value: 1107,
       path_stroke_dashoffset_multiply_value: 1,
+      // cover path vaule
+      cover_path_fill_value: '#fff',
       // svg path values
-      main_path_stroke_width_value: 1,
-      main_path_stroke_color_value: '#fff',
-      main_path_fill_value: 'none',
-      // animation values
-      ani_path_stroke_dasharray_value: '',
-      ani_path_stroke_width_value: 1,
-      ani_path_stroke_color_value: '#fff',
-      ani_path_fill_value: '',
-      ani_reverse_path_stroke_width_value: 1,
-      ani_reverse_path_stroke_color_value: '#fff',
-    },
-    {
-      id: 'loc2',
-      direction: 'forward',
-      translate_value: '135, 40',
-      scale_value: '1, 1',
-      path_stroke_dasharray_value: 1107,
-      path_stroke_dashoffset_multiply_value: 0,
-      main_path_stroke_width_value: 9,
+      main_path_stroke_width_value: 8,
       main_path_stroke_color_value: 'red',
       main_path_fill_value: 'none',
-      path_d_value: `M2.59,108.2c2.85,4.89,141.15,69.24,155.41,73.52,14.26,4.28,62.02,19.25,71.29,22.1s119.05,60.59,190.34,71.29c71.29,10.69,163.25-17.11,189.2-27.8,25.95-10.69,172.23-102.65,172.23-102.65L1003.15,2.53`,
+      main_path_opacity: 1,
+      // reverse path values
+      reverse_path_stroke_width_value: 9,
+      reverse_path_stroke_color_value: 'black',
+      reverse_path_fill_value: 'none',
+      reverse_path_opacity: 1,
+      // ani path values
+      ani_path_stroke_dasharray_split_value: 120,
+      ani_path_stroke_width_value: 8,
+      ani_path_stroke_color_value: '#971717',
+      ani_path_fill_value: 'none',
+      ani_path_opacity: 1,
+      // animation values
+      animation_timing_function: 'linear',
+      animation_iteration_count: 'infinite',
+      animation_speed: 1.25,
     },
   ];
 
@@ -121,9 +141,9 @@ const SvgContainer = ({ percent1, percent2 }) => {
   return (
     <SvgArea>
       {/* 최상위 SVG 태그, viewBox 지정 */}
-      <svg className="map-svg" viewBox="0 0 100% 100%">
-        <PathComponent pathOption={movingPathOptions[1]} percent={percent2} />
-        <PathAnimation pathOption={movingPathOptions[1]} percent={percent2} />
+      <svg className="map-svg" viewBox="0 0 1384 360">
+        <PathComponent pathOption={movingPathOptions[0]} percent={percent2} />
+        {/* <PathAnimation pathOption={movingPathOptions[1]} percent={percent2} /> */}
       </svg>
     </SvgArea>
   );
